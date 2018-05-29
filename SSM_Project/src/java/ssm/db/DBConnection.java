2f874ebe049c0430c21a7bff731ac9d2cd280c3e
minhhy2801 <minhhy2897@gmail.com>
2018-05-29 09:09:15 +0700

    Create project

---
+++ SSM_Project/src/java/ssm/db/DBConnection.java
@@ -0,0 +1,18 @@
+package ssm.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+  public static Connection getConnection() {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3000;databaseName=", "", "");
+      return conn;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+}
