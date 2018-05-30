b86d8a041246c0960998ae6c8489a13227960b25
thupnm <minh.thu2831997@gmail.com>
2018-05-30 18:51:36 +0700

    Login, Forgot Password

---
+++ SSM_Project/src/java/ssm/controllers/LoginController.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ssm.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import ssm.dao.AccountDAO;
+import ssm.dto.AccountDTO;
+
+/**
+ *
+ * @author ThuPMNSE62369
+ */
+public class LoginController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        try (PrintWriter out = response.getWriter()) {
+            String email = request.getParameter("txtEmail");
+            String password = request.getParameter("txtPassword");
+            AccountDAO dao = new AccountDAO();
+            int role = dao.checkLogin(email, password);
+            //ArrayList<RegistrationDTO> list = new ArrayList<>();
+            AccountDTO dto = new AccountDTO();
+            dto = dao.find(email);
+            session.setAttribute("INFO", dto);
+            if (role == 1) {
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+            } else if (role == 2) {
+                request.setAttribute("INVALID", "Invalid username or password. Please try again!");
+            } 
+        } catch (Exception e) {
+            log("ERROR at LoginController" + e.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
