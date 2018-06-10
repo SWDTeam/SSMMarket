5fe3537e316b1e5ba9c6d9935b6681666a53df4a
tuankiet1797@gmai.com <01676243500>
2018-06-10 23:53:03 +0700

    Fifth upload

--- SSM_Project/src/java/kietpt/dao/ProductDAO.java
+++ SSM_Project/src/java/kietpt/dao/ProductDAO.java
@@ -107,4 +107,45 @@ public class ProductDAO {
         }
         return listProduct;
     }
+
+    public List<ProductDTO> getListProductByCateId(int id) {
+        Connection conn = null;
+        PreparedStatement preStm = null;
+        ResultSet rs = null;
+        List<ProductDTO> listProduct = new ArrayList<>();
+        try {
+            conn = DBConnection.getConnection();
+            String sql = "select pro.productId,pro.productKey,pro.productName,pro.price,pro.description, img.imgKey "
+                    + "from Product pro, Images img "
+                    + "where pro.categoryId = ? and pro.productId = img.productId ";
+            preStm = conn.prepareStatement(sql);
+            preStm.setInt(1, id);
+            rs = preStm.executeQuery();
+            String name = "", des = "", urlPic = "", productKey = "";
+            int proId;
+            float price;
+            while (rs.next()) {
+                proId = rs.getInt("productId");
+                name = rs.getString("productName");
+                des = rs.getString("description");
+                urlPic = rs.getString("imgKey");
+                price = rs.getFloat("price");
+                productKey = rs.getString("productKey");
+                ProductDTO dto = new ProductDTO();
+                dto.setProductId(id);
+                dto.setProductName(name);
+                dto.setDescription(des);
+                dto.setUrlPic(urlPic);
+                dto.setPrice(price);
+                dto.setProductKey(productKey);
+                listProduct.add(dto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection(conn, preStm, rs);
+        }
+        return listProduct;
+    }
+
 }
