d6b951a5848b7ba9fbbf48e0cdee82a6e5d11967
thupnm <minh.thu2831997@gmail.com>
2018-06-11 17:05:24 +0700

    update connection

--- SSM_Project/src/java/ssm/db/DBConnection.java
+++ SSM_Project/src/java/ssm/db/DBConnection.java
@@ -7,7 +7,7 @@ public class DBConnection {
   public static Connection getConnection() {
     try {
       Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-      Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ssmmarket", "sa", "sa12345678");
+      Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=ssmmarket", "sa", "se62369");
       return conn;
     } catch (Exception e) {
       e.printStackTrace();
